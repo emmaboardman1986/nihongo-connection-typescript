d84fde8f710d3fce34ce43886b17c4f4042edb40
E2 <e2@macbook-pro-5.home>
2020-05-22 16:52:41 +0100

    update mailto link

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -32,7 +32,7 @@ const ContactPage: React.FC<Props> = () => {
             <VerticalSpacing size={{ _: "baseTight" }}></VerticalSpacing>
             <SocialLinks></SocialLinks>
             <VerticalSpacing size={{ _: "tight" }}></VerticalSpacing>
-            <TextLink link="mailto:nihongoconnection@gmail.com">
+            <TextLink link="mailto:nihongoconnection@gmail.com" isExternal>
               NihongoConnection@gmail.com
             </TextLink>
           </FlexContainer>
